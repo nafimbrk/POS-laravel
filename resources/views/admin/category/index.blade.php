6d9d3ce8066745fa92e7b87b3152c4fed63d1a93
nafimbrk <nafimubarok2005@gmail.com>
2025-05-22 05:51:42 -0700

    add query string at category

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -58,6 +58,7 @@
             <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                 <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
+                        <th scope="col" class="px-6 py-3">No</th>
                         <th scope="col" class="px-6 py-3">Nama Kategori</th>
                         <th scope="col" class="px-6 py-3">Aksi</th>
                     </tr>
@@ -65,6 +66,9 @@
                 <tbody>
                     @foreach ($categories as $category)
                     <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            {{ $loop->iteration + ($categories->currentPage() - 1) * $categories->perPage() }}
+                        </th>
                         <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                             {{ $category->name }}
                         </th>
@@ -93,7 +97,7 @@
 
 
     </div>
-    {{ $categories->links() }}
+    {{ $categories->withQueryString()->links() }}
 </div>
 </div>
 </x-layout>
--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -62,6 +62,9 @@
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
                 <th scope="col" class="px-6 py-3">
+                    No
+                </th>
+                <th scope="col" class="px-6 py-3">
                     Gambar
                 </th>
                 <th scope="col" class="px-6 py-3">
@@ -84,6 +87,9 @@
         <tbody>
             @foreach ($products as $product)
             <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    {{-- {{ $loop->iteration + ($products->currentPage() - 1) * $products->perPage() }} --}}
+                </th>
                 <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                     @if ($product->image)
                     <img src="{{ asset('storage/image/' . $product->image) }}" alt="" width="150" class="rounded-lg">
